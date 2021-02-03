88ba9dcb21a06a1656ee75a21e67c11e4306c4d0
Huguez <carlos.huguez@gmail.com>
2021-02-02 20:36:28 -0700

    se agregaron atributos y se pasaron en app-grafica

--- src/app/pages/deudas/deudas.component.ts
+++ src/app/pages/deudas/deudas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-deudas',
@@ -7,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DeudasComponent implements OnInit {
+  
+  public data = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C' }
+  ];  
+
+  public label= [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
 
   constructor() { }
 
