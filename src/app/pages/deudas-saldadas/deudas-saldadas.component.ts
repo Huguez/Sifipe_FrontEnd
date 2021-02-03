fffee5612877730ee675d3b3d706bb735404e36f
Huguez <carlos.huguez@gmail.com>
2021-02-02 18:57:41 -0700

    se creo las paginas para las deudas

---
+++ src/app/pages/deudas-saldadas/deudas-saldadas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deudas-saldadas',
+  templateUrl: './deudas-saldadas.component.html',
+  styles: [
+  ]
+})
+export class DeudasSaldadasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/deudas/deudas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deudas',
+  templateUrl: './deudas.component.html',
+  styles: [
+  ]
+})
+export class DeudasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
