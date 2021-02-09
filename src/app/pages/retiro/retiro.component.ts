b7a0c8966638933223a30c3b3aebd5b6aea17fcd
Huguez <carlos.huguez@gmail.com>
2021-02-08 23:09:36 -0700

    se creo la page para el ahorro de retiro

---
+++ src/app/pages/retiro/retiro.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retiro',
+  templateUrl: './retiro.component.html',
+  styles: [
+  ]
+})
+export class RetiroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
