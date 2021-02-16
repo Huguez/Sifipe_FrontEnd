32105083c88634260c095cb5056b13fc56e77b46
Huguez <carlos.huguez@gmail.com>
2021-02-16 14:16:05 -0700

    nueva vista para el dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  
+  public gastoEfectuado:   number = 100;
+  public gastoActual:      number = 100; // ?
 
+  public ingresoAcumulado: number = 100;
+  public ingresoDisponible:number = 100;
+
+  public inversion:        number = 100;
+  public montoInvertido:   number = 100
+
+  public data =  [ 500, 350 ];
+  public label =  [ ['In', 'Store', 'Sales'], 'Mail Sales'];
+  
+
+  
   constructor() { }
 
   ngOnInit(): void {
