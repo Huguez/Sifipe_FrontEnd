1e4ef60ce5f1c5080fd775cf489d40b3934533b1
Huguez <carlos.huguez@gmail.com>
2021-02-16 16:53:01 -0700

    se implemento la primera vista de la page inversiones

--- src/app/pages/inversiones/inversiones.component.ts
+++ src/app/pages/inversiones/inversiones.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-inversiones',
   templateUrl: './inversiones.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class InversionesComponent implements OnInit {
 
-  constructor() { }
+  public data =  [30, 15, 20, 15, 20];
+  public label =  ['VOO', 'VEA', 'VNQ', 'CETES', 'fibras' ];
+  
+
+  public cabecera = ['Periodo', 'I1' , 'I2' , 'I3' , 'Total'];
+  public cuerpo = [[ 'enero-2020', 750.00,750.00,750.00, 750.00 ]];
+  
+  public inversionForm:FormGroup;
+
+  constructor( private _fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.inversionForm = this._fb.group( {
+      categoria: new FormControl( null, [ Validators.required ] ),
+      subCategoria: new FormControl( null, [ Validators.required ] ),
+      monto: new FormControl( null, [ Validators.required, Validators.min( 1.0 ) ] )
+    } );
   }
 
+  ingresarInversion(){
+    console.log( this.inversionForm.value );
+    
+  }
+  
+  prueba(){
+    
+  }
 }
